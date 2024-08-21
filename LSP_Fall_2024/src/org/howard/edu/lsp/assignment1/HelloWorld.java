75fc47fc50cdc735d9a61e926bba286603cd45a5
bpwoolfolk <bpwoolfolk@hotmail.com>
2024-08-20 20:17:23 -0400

    Second commit.

--- LSP_Fall_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -3,6 +3,6 @@ package org.howard.edu.lsp.assignment1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello LSP!!!");
+		System.out.println("Hello Prof. Woolfolk!!!");
 	}
 }
